ccfb2374de554aadc2d10d6bf7b15df903c7e98a
Kaushik Sriram <kaushik162000@gmail.com>
2021-05-07 20:19:37 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookMarkComponent } from './components/book-mark/book-mark.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookMarkComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/book-mark/book-mark.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-mark',
+  templateUrl: './book-mark.component.html',
+  styleUrls: ['./book-mark.component.css']
+})
+export class BookMarkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
