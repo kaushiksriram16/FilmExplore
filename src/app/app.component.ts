51aa97fa78c978854d2949ee87a6cbbe544ac7c7
Kaushik Sriram <kaushik162000@gmail.com>
2021-05-10 19:12:53 +0530

    home-page changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,26 @@ import { ChangeDetectionStrategy, Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
-
 export class AppComponent {
-  title = 'fetchMoviesApp';
   movieTitle: string;
+
+  ApiLink =
+    'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=57f0bfdee1dd128bce2040054c456112';
+
+  ImgLink =
+    'https://image.tmbd.org/t/p/w500&api_key=57f0bfdee1dd128bce2040054c456112';
+
+  PopularMovies: any[];
+
+  getMovieData() {
+    fetch(this.ApiLink)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.PopularMovies = data.results;
+        console.log(this.PopularMovies);
+      });
+  }
 }
 
